4a1847dcf8687daa66ba2de3e8ba127d263d8019
Isabel Muñoz <ishamu023@gmail.com>
2021-10-17 12:51:55 -0300

    Ejercicio agregar valores por defecto a través de código a nombre y apellido

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.constraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView ivAvatar;
+    private EditText etNombre;
+    private EditText etApellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initWidgets();
         loadImage();
+        loadName();
+        loadApellido();
+    }
+
+    private void loadApellido() {
+        etApellido.setText("Muñoz");
+    }
+
+    private void loadName() {
+        etNombre.setText("Isabel");
     }
 
     private void initWidgets() {
         ivAvatar=findViewById(R.id.iv_avatar);
+        etNombre=findViewById(R.id.et_nombre);
+        etApellido=findViewById(R.id.et_apellido);
     }
 
     private void loadImage() {
